758eaa0b72b8ff7a931c422d95a835d304559202
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-08-23 13:55:38 +0200

    login works
    only with hard-coded email and password

--- inc/action_login.php
+++ inc/action_login.php
@@ -1,14 +1,17 @@
-<?php
+<?php session_start();
 include_once "class.user.php";
 
 $user = new User();
 
-if(isset($_POST['submit'])) {
+//if($_POST['submit']) {
 
     //protect from malicious insertation
-    $email = $user->protect_input($email, 'email');
-    $pass =$user->protect_input($pass, 'pass');
-        $passhash = hash('sha256', $pass);
+    // $email = $user->protect_input($email, 'email');
+    // $pass = $user->protect_input($pass, 'pass');
+    //     $passhash = hash('sha256', $pass);
+    $email = "agyarmati123@gmail.com";
+    $pass = "12369874";
+    $passhash = hash('sha256', $pass);
 
     //check if data is already in database
     $check = $user->check_credentials('users', 'email', $email);
@@ -25,4 +28,4 @@ if(isset($_POST['submit'])) {
         echo "Incorrect email or password";
     }
  
-}//$_POST close tag
+//}//$_POST close tag
--- inc/class.user.php
+++ inc/class.user.php
@@ -1,8 +1,4 @@
 <?php
-if(isset($_SESSION['user'])!="") {
-    header("Location: login.php");
-    exit;
-}
 class User {
 
 ////////////////////////////////////check if more credentials are already exists//////////////////
